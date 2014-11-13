3dc1796ca5b5832f1bf2b6840fb758d137bcfe06
Michel Bystranowski <michel@vertige.org>
2014-11-13 16:36:18 +0100

    commit initial

---
+++ lang/paquet-ckeditor_fix_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'ckeditor_fix_description' => 'Les liens au format SPIP sont transformés en entités html par ckeditor, ce qui casse tout… Le problème vient du chargement en ajax de la div contenu, alors on ne cherche pas et on désajaxifie cette div.',
+	'ckeditor_fix_nom' => 'CKEditor Fix',
+	'ckeditor_fix_slogan' => 'un moyen détourné de régler un bug avec CKEditor…',
+);
+
+?>
